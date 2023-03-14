e2eae1501a0efd18bbd624fc86df5a6d6b218f38
olubiyi09 <olubiyi09@gmail.com>
2023-03-14 15:10:30 +0400

    revert the changes made to the index js file

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router history={browserHistory}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
